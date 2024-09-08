0b10f60763c92dc481eb980564250d8d1dedd5c1
efe.kutas <efe.kutas@sans-technology.com>
2024-09-08 19:28:35 +0300

    config ayaraı

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,8 @@ export default {
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  buildDir: 'dist', // Varsayılan olarak 'dist' olarak ayarlanmış olmalı
+  generate: {
+    dir: 'dist', // Uygulamanızın üretim çıkış dizini
+  },
 }
